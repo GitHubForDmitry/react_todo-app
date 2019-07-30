5881fcaa33c6ae442b6b7c75b9b13b98d80c7d2a
GitHubForDmitry <dmitrykoka@gmail.com>
2019-07-30 21:43:05 +0300

    Updates

--- precache-manifest.52c2b91d4a110bcc86afea60cae7cfaa.js
+++ precache-manifest.52c2b91d4a110bcc86afea60cae7cfaa.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d59fe9456325f62867cebf920194c262",
+    "revision": "2fe392c7bce1e8bdc85036ad1d24693e",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "e3a7ae9182cb078951dc",
+    "revision": "bde260f2ccff193b0aa5",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "9122ca913536010c15fa",
-    "url": "/react_todo-app/static/js/2.ed9bf3e2.chunk.js"
+    "revision": "529854f14605209346dd",
+    "url": "/react_todo-app/static/js/2.37a9ca8a.chunk.js"
   },
   {
-    "revision": "e3a7ae9182cb078951dc",
-    "url": "/react_todo-app/static/js/main.3885d7e7.chunk.js"
+    "revision": "bde260f2ccff193b0aa5",
+    "url": "/react_todo-app/static/js/main.b33d2a74.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.6567ae03b130be88b3e226a212aea46f.js"
+  "/react_todo-app/precache-manifest.52c2b91d4a110bcc86afea60cae7cfaa.js"
 );
 
 self.addEventListener('message', (event) => {
